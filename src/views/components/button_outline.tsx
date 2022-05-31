ddd5881a19eb00908187624b4f365e717af98b33
MohamadReza66666 <heydari.more@gmail.com>
2022-05-31 10:17:21 -0700

    Add <create component ButtonOutLine and add styles>

---
+++ src/views/components/button_outline.tsx
@@ -0,0 +1,32 @@
+import {
+    memo,
+    ReactElement,
+    useCallback,
+    MouseEvent,
+    ButtonHTMLAttributes
+} from "react";
+
+interface Props extends ButtonHTMLAttributes<HTMLButtonElement> {
+    title: string;
+}
+
+export function ButtonOutline({ onClick, title }: Props): ReactElement {
+
+    const handleOnClick = useCallback((event: MouseEvent<HTMLButtonElement>) => {
+        event.stopPropagation();
+        onClick?.(event);
+    }, [onClick]);
+
+    return (
+        <button
+            className="button-outline"
+            onClick={handleOnClick}
+        >
+            <h3 className="button-outline-text">
+                {title}
+            </h3>
+        </button>
+    )
+}
+
+export default memo(ButtonOutline);
