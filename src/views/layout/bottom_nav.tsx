953b655f5a593cc5234f83f0b73fe1037e1555bf
MohamadReza66666 <heydari.more@gmail.com>
2022-06-01 14:20:47 -0700

    Add <add responsive design>

--- src/views/layout/bottom_nav.tsx
+++ src/views/layout/bottom_nav.tsx
@@ -1,5 +1,7 @@
-import { useState } from "react";
+import { useCallback, useState, MouseEvent } from "react";
+import { Link } from "react-router-dom";
 import {
+    IconBar,
     IconDot,
     ImageDropDown1,
     ImageDropDown2,
@@ -13,9 +15,11 @@ import {
 import {
     Button,
     ButtonDropdown,
+    DropDown,
     Hr,
     SelectCategories,
-    TextIconHorizontal
+    TextIconHorizontal,
+    TextIconVertical
 } from "../components/components";
 
 const TEST = [
@@ -40,46 +44,129 @@ const ITEM_DROPDOWN = [
 export function BottomNav() {
 
     const [showDropDown, setShowDropDown] = useState(false);
+    const [showMinimalDropDown, setShowMinimalDropDown] = useState(false);
     const [active, setActive] = useState(0);
 
     function handleshowDropDown(status: boolean) {
         setShowDropDown(status);
-    }
+    };
 
     function handleCloseDropDown() {
         setShowDropDown(false);
         setActive(0);
-    }
+    };
 
     function handleActiveLink(id: number) {
         setActive(id);
-    }
-
+    };
+    const handleshowMinimalDropDown = useCallback((event: MouseEvent<HTMLElement>) => {
+        setShowMinimalDropDown(!showMinimalDropDown)
+    }, [showMinimalDropDown]);
     return (
-        <div className="bottom-nav">
-            <div className="bottom-nav-search">
-                <SelectCategories onOpen={handleshowDropDown} />
-            </div>
-            <div className="bottom-nav-links">
-                <Button title={"EXCESS CAPACITY"} />
-                <Button title={"PRODUCT"} />
-                <ButtonDropdown title={"SUPPLIERS"} links={TEST} />
-                <ButtonDropdown title={"DEMANDERS"} links={[]} />
-                <ButtonDropdown title={"BROKERS"} links={[]} />
-                <ButtonDropdown title={"SIDE SERVICES"} links={[]} />
-                <Button title={"SPECIAL OFFERS"} />
-                <Button title={"MAP"} />
-            </div>
-            { showDropDown === true ? (
-                <div className="bottom-nav-dropdown" onMouseLeave={handleCloseDropDown}>
-                    <div className="section-rectangle">
-                        <div className="section-triggle">
+        <>
+            <div className="bottom-nav">
+                <div className="bottom-nav-search">
+                    <SelectCategories onOpen={handleshowDropDown} />
+                </div>
+                <div className="bottom-nav-links">
+                    <Button title={"EXCESS CAPACITY"} />
+                    <Button title={"PRODUCT"} />
+                    <ButtonDropdown title={"SUPPLIERS"} links={TEST} />
+                    <ButtonDropdown title={"DEMANDERS"} links={[]} />
+                    <ButtonDropdown title={"BROKERS"} links={[]} />
+                    <ButtonDropdown title={"SIDE SERVICES"} links={[]} />
+                    <Button title={"SPECIAL OFFERS"} />
+                    <Button title={"MAP"} />
+                </div>
+                {showDropDown === true ? (
+                    <div className="bottom-nav-dropdown" onMouseLeave={handleCloseDropDown}>
+                        <div className="section-rectangle">
+                            <div className="section-triggle">
+                            </div>
+                            <div className="content">
+                                <div className="content-items">
+                                    {ITEM_DROPDOWN.map(name => (
+                                        <div key={name.id}>
+                                            <TextIconHorizontal
+                                                icon={name.icon}
+                                                title={name.title}
+                                                showArrow={true}
+                                                active={active === name.id ? true : false}
+                                                onClick={() => handleActiveLink(name.id)}
+                                            />
+                                            <Hr />
+                                        </div>
+                                    ))}
+                                </div>
+                                <div className="content-divider">
+
+                                </div>
+                                <div className="content-submenus">
+                                    <div className="content-submenus-section">
+                                        <div className="content-submenus-section-header">
+                                            <h4 className="content-submenus-section-header-text">
+                                                Title Here
+                                            </h4>
+                                        </div>
+                                        <div className="content-submenus-section-body">
+                                            <TextIconHorizontal icon={IconDot} title={"Sub-Menu-Here"} showArrow={false} active={false} />
+                                            <TextIconHorizontal icon={IconDot} title={"Sub-Menu-Here"} showArrow={false} active={false} />
+                                            <TextIconHorizontal icon={IconDot} title={"Sub-Menu-Here"} showArrow={false} active={false} />
+                                            <TextIconHorizontal icon={IconDot} title={"Sub-Menu-Here"} showArrow={false} active={false} />
+                                            <TextIconHorizontal icon={IconDot} title={"Sub-Menu-Here"} showArrow={false} active={false} />
+                                            <TextIconHorizontal icon={IconDot} title={"Sub-Menu-Here"} showArrow={false} active={false} />
+                                        </div>
+                                    </div>
+                                    <div className="content-submenus-section">
+                                        <div className="content-submenus-section-header">
+                                            <h4 className="content-submenus-section-header-text">
+                                                Title Here
+                                            </h4>
+                                        </div>
+                                        <div className="content-submenus-section-body">
+                                            <TextIconHorizontal icon={IconDot} title={"Sub-Menu-Here"} showArrow={false} active={false} />
+                                            <TextIconHorizontal icon={IconDot} title={"Sub-Menu-Here"} showArrow={false} active={false} />
+                                            <TextIconHorizontal icon={IconDot} title={"Sub-Menu-Here"} showArrow={false} active={false} />
+                                            <TextIconHorizontal icon={IconDot} title={"Sub-Menu-Here"} showArrow={false} active={false} />
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
-                        <div className="content">
-                            <div className="content-items">
+                    </div>
+                ) : (null)}
+            </div>
+            <div className="bottom-nav-minimal">
+                <button
+                    className="border-0"
+                    type="button"
+                    data-bs-toggle="offcanvas"
+                    data-bs-target="#offcanvasMinimal"
+                    aria-controls="offcanvasMinimal"
+                >
+                    <img src={IconBar} className="w-100" />
+                </button>
+                <div className="offcanvas offcanvas-start" id="offcanvasMinimal" aria-labelledby="offcanvasTopLabel">
+                    <div className="offcanvas-header">
+                        <h5 id="offcanvasTopLabel"></h5>
+                        <button type="button" className="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                    </div>
+                    <div className="offcanvas-body">
+                        <Link to="/">EXCESS CAPACITY</Link>
+                        <Link to="/">PRODUCT</Link>
+
+                        <DropDown title={"SUPPLIERS"} links={TEST} />
+                        <DropDown title={"DEMANDERS"} links={[]} />
+                        <DropDown title={"BROKERS"} links={[]} />
+                        <DropDown title={"SIDE SERVICES"} links={[]} />
+                        <Link to="/">SPECIAL OFFERS</Link>
+                        <Link to="/">MAP</Link>
+                        <DropDown title={"CATEGORIES"} links={[]} onClick={handleshowMinimalDropDown} />
+                        {showMinimalDropDown === true ? (
+                            <div>
                                 {ITEM_DROPDOWN.map(name => (
                                     <div key={name.id}>
-                                        <TextIconHorizontal
+                                        <TextIconVertical
                                             icon={name.icon}
                                             title={name.title}
                                             showArrow={true}
@@ -90,44 +177,11 @@ export function BottomNav() {
                                     </div>
                                 ))}
                             </div>
-                            <div className="content-divider">
-
-                            </div>
-                            <div className="content-submenus">
-                                <div className="content-submenus-section">
-                                    <div className="content-submenus-section-header">
-                                        <h4 className="content-submenus-section-header-text">
-                                            Title Here
-                                        </h4>
-                                    </div>
-                                    <div className="content-submenus-section-body">
-                                        <TextIconHorizontal icon={IconDot} title={"Sub-Menu-Here"} showArrow={false} active={false} />
-                                        <TextIconHorizontal icon={IconDot} title={"Sub-Menu-Here"} showArrow={false} active={false} />
-                                        <TextIconHorizontal icon={IconDot} title={"Sub-Menu-Here"} showArrow={false} active={false} />
-                                        <TextIconHorizontal icon={IconDot} title={"Sub-Menu-Here"} showArrow={false} active={false} />
-                                        <TextIconHorizontal icon={IconDot} title={"Sub-Menu-Here"} showArrow={false} active={false} />
-                                        <TextIconHorizontal icon={IconDot} title={"Sub-Menu-Here"} showArrow={false} active={false} />
-                                    </div>
-                                </div>
-                                <div className="content-submenus-section">
-                                    <div className="content-submenus-section-header">
-                                        <h4 className="content-submenus-section-header-text">
-                                            Title Here
-                                        </h4>
-                                    </div>
-                                    <div className="content-submenus-section-body">
-                                        <TextIconHorizontal icon={IconDot} title={"Sub-Menu-Here"} showArrow={false} active={false} />
-                                        <TextIconHorizontal icon={IconDot} title={"Sub-Menu-Here"} showArrow={false} active={false} />
-                                        <TextIconHorizontal icon={IconDot} title={"Sub-Menu-Here"} showArrow={false} active={false} />
-                                        <TextIconHorizontal icon={IconDot} title={"Sub-Menu-Here"} showArrow={false} active={false} />
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+                        ) : (null)}
                     </div>
                 </div>
-            ) : (null)}
-        </div>
+            </div>
+        </>
     )
 }
 
