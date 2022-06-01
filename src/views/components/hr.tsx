d77b1165f9ee8a5e5a227ada45a78e8f60330559
MohamadReza66666 <heydari.more@gmail.com>
2022-06-01 11:15:58 -0700

    Add <create component Hr and add styles>

---
+++ src/views/components/hr.tsx
@@ -0,0 +1,11 @@
+import { ReactElement, memo } from "react";
+
+export function Hr(): ReactElement {
+
+    return (
+        <div className="hr">
+            
+        </div>
+    )
+}
+export default memo(Hr);
