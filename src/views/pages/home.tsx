f295e7ca15452ad29ccd1aa19ae5fe390a02a82b
MohamadReza66666 <heydari.more@gmail.com>
2022-06-03 00:46:20 -0700

    Add <add section earn to page Home and add style>

--- src/views/pages/home.tsx
+++ src/views/pages/home.tsx
@@ -1,6 +1,6 @@
 import { Component } from "react";
-import { ImagePersonBox } from "../../assets";
-import { CardSearchProduct } from "../components/components";
+import { ImageLaboratory, ImagePersonBox } from "../../assets";
+import { Button, CardSearchProduct } from "../components/components";
 import HomeController from "../controllers/home_controller";
 
 
@@ -27,6 +27,36 @@ class Home extends HomeController {
                     </div>
                     <div className="home-header-inputs">
                         <CardSearchProduct />
+                    </div>
+                </div>
+                <div className="home-section-earn">
+                    <div className="home-section-earn-content">
+                        <h4 className="home-section-earn-content-header">
+                            Let`s earn from
+                        </h4>
+                        <h2 className="home-section-earn-content-title">
+                            Excess Capacity
+                        </h2>
+                        <div className="home-section-earn-content-description">
+                            <p className="home-section-earn-content-description-text">
+                                Lorem ipsum dolor sit amet,
+                                consectetuer adipiscing elit,
+                                sed diam nonummy nibh euismod
+                                tincidunt ut laoreet dolore
+                                magna aliquam erat volutpat.
+                                Ut wisi enim ad minim veniam,
+                                quis nostrud exerci tation
+                                ullamcorper suscipit lobortis
+                                nisl ut aliquip ex ea commodo
+                                consequat. Duis autem vel eum
+                                iriure dolor in hendrerit in
+                                vulputate
+                            </p>
+                        </div>
+                        <Button title="LEARN MORE" />
+                    </div>
+                    <div className="home-section-earn-image">
+                        <img src={ImageLaboratory} className="image-laboratory" />
                     </div>
                 </div>
             </div>
