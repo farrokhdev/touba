956c1bea1cfbb91d1b522317b9ef2482f5298690
psycho <mahdi.s.1380.315@gmail.com>
2022-06-04 11:29:53 +0430

    component search bar

--- src/views/components/component_search_bar.tsx
+++ src/views/components/component_search_bar.tsx
@@ -6,16 +6,16 @@ import { InputTextLabel } from "./input_text_label";
 const ComponentSearchBar = () => {
   return (
     <div className="d-flex align-items center row p-4 bg-white">
-      <div className="col-3 p-2">
+      <div className="col-xl-3 col-sm-12 p-2">
         <InputTextLabel value={""} />
       </div>
-      <div className="col-3 p-2">
+      <div className="col-xl-3 col-sm-12 p-2">
         <SelectText value={""} />
       </div>
-      <div className="col-3 p-2 justify-content-center">
+      <div className="col-xl-3 col-sm-12 p-2 justify-content-center">
         <SelectComponent type='one'  />
       </div>
-      <div className="col-3 p-2">
+      <div className="col-xl-3 col-sm-12 p-2">
         <ButtonComponent title="search"  />
       </div>
     </div>
--- src/views/pages/SearchResultExcessCapacity/SearchResultExcessCapacity.tsx
+++ src/views/pages/SearchResultExcessCapacity/SearchResultExcessCapacity.tsx
@@ -1,9 +1,11 @@
 import React, { Component } from "react";
 import { BaseComponent } from "utilities_js";
+import { CardOfferHorizontal } from "../../components/cars_offer_horizontal";
 import Component_search_bar from "../../components/component_search_bar";
 import BottomNav from "../../layout/bottom_nav";
 import Footer from "../../layout/footer";
 import TopNav from "../../layout/top_nav";
+import image_test from "../../../assets/images/image-test.png";
 interface IProps {}
 
 interface IState {
@@ -11,13 +13,91 @@ interface IState {
 }
 
 export class SearchResultExcessCapacity extends BaseComponent<IProps, IState> {
+  TEST = [
+    { name: "JAN", size: 20 },
+    { name: "FEB", size: 30 },
+    { name: "MAR", size: 40 },
+    { name: "APR", size: 50 },
+    { name: "MAY", size: 60 },
+    { name: "JUN", size: 70 },
+    { name: "JUL", size: 80 },
+    { name: "AUG", size: 90 },
+    { name: "SEP", size: 100 },
+    { name: "OCT", size: 20 },
+    { name: "NOV", size: 20 },
+    { name: "DEC", size: 20 },
+  ];
   render() {
     return (
       <div>
         <TopNav />
         <BottomNav />
-        <Component_search_bar/>
-        <Footer/>
+        <Component_search_bar />
+        <div className=" container">
+          <div className="row p-3      ">
+            <div className="col-2">s</div>
+            <div className="col-10">
+              <div className="p-2">
+                <CardOfferHorizontal
+                  image={image_test}
+                  status={"exp"}
+                  description={
+                    "Pomegranate Juice Pet Bottle, Best material and design, Ready to Order"
+                  }
+                  rate={4}
+                  country={"chin"}
+                  chart={this.TEST}
+                  amount={5}
+                  discountPercent={2}
+                />
+              </div>
+              <div className="p-2">
+                <CardOfferHorizontal
+                  image={image_test}
+                  status={"exp"}
+                  description={
+                    "Pomegranate Juice Pet Bottle, Best material and design, Ready to Order"
+                  }
+                  rate={4}
+                  country={"chin"}
+                  chart={this.TEST}
+                  amount={5}
+                  discountPercent={2}
+                />
+              </div>
+              <div className="p-2">
+                <CardOfferHorizontal
+                  image={image_test}
+                  status={"exp"}
+                  description={
+                    "Pomegranate Juice Pet Bottle, Best material and design, Ready to Order"
+                  }
+                  rate={4}
+                  country={"chin"}
+                  chart={this.TEST}
+                  amount={5}
+                  discountPercent={2}
+                />
+              </div>
+              <div className="p-2">
+                <CardOfferHorizontal
+                  image={image_test}
+                  status={"exp"}
+                  description={
+                    "Pomegranate Juice Pet Bottle, Best material and design, Ready to Order"
+                  }
+                  rate={4}
+                  country={"chin"}
+                  chart={this.TEST}
+                  amount={5}
+                  discountPercent={2}
+                />
+              </div>
+             
+            </div>
+          </div>
+        </div>
+        <Footer />
       </div>
     );
   }
