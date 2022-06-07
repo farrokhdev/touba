1db233062d8498e520a67a5da0560a758e47c792
MRezaHeydar1376 <103419484+MRezaHeydar1376@users.noreply.github.com>
2022-06-06 22:02:37 -0700

    Amend <update component InputLabelText>

--- src/views/components/card_search_product.tsx
+++ src/views/components/card_search_product.tsx
@@ -28,18 +28,18 @@ export function CardSearchProduct({ }: Props): ReactElement {
                         key={button.id}
                         className={
                             active === button.id ?
-                            "card-search-product-header-button active-border"
-                            : 
-                            "card-search-product-header-button"
+                                "card-search-product-header-button active-border"
+                                :
+                                "card-search-product-header-button"
                         }
-                        onClick={() => {handleActiveButton(button.id)}}
+                        onClick={() => { handleActiveButton(button.id) }}
                     >
                         <p
                             className={
-                                active === button.id ? 
-                                "card-search-product-header-button-text active"
-                                :
-                                "card-search-product-header-button-text"
+                                active === button.id ?
+                                    "card-search-product-header-button-text active"
+                                    :
+                                    "card-search-product-header-button-text"
                             }
                         >
                             {button.name}
@@ -52,7 +52,14 @@ export function CardSearchProduct({ }: Props): ReactElement {
                     Search for free product lines to make a product with your brand identity
                 </h6>
                 <div className="card-search-product-inputs-group">
-                    <InputTextLabel value={search} onChange={(event => {setSearch(event.currentTarget.value)})}/>
+                    <InputTextLabel
+                        value={search}
+                        onChange={(event => { setSearch(event.currentTarget.value); })}
+                        title={"Product or Service"}
+                        placeholder={"What product you want to produce? Eg. PET Bottle"}
+                        type={"text"}
+                        showDropDown={true}
+                    />
                     <SelectText value={""} />
                     <SelectComponent type={"one"} />
                     <Button title="SEARCH" />
