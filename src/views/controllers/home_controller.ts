707b800fecc128a4ef6bc98b78520609b8a88694
MRezaHeydar1376 <103419484+MRezaHeydar1376@users.noreply.github.com>
2022-06-14 04:23:18 -0700

    Add <add new states and functions to get result CardSearch from page Home>

--- src/views/controllers/home_controller.ts
+++ src/views/controllers/home_controller.ts
@@ -6,17 +6,41 @@ interface IProps {
 
 interface IState {
     more: boolean;
+    selectText: string;
+    select: string;
+    valueText: string;
+    search: string;
 }
 
 export class HomeController extends Component<IProps, IState> {
 
     state = {
         more: false,
+        selectText: "",
+        select: "",
+        valueText: "",
+        search: "",
     };
 
     //list
     handleBlurText = () => {
-        this.setState({more: true});
+        this.setState({ more: true });
+    }
+    handleGetSelectText = (value: string) => {
+        console.log(value);
+        this.setState({ selectText: value });
+    }
+    handleGetSelect = (value: string) => {
+        console.log(value);
+        this.setState({ select: value });
+    }
+    handleGetValueText = (value: string) => {
+        console.log(value);
+        this.setState({ valueText: value });
+    }
+    handleGetResultSearch = (value: string) => {
+        console.log(value);
+        this.setState({ search: value });
     }
 }
 export default HomeController;
