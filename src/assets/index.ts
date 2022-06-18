947cfe9ac69f8abf872b83a104fb0168ef81a076
MRezaHeydar1376 <103419484+MRezaHeydar1376@users.noreply.github.com>
2022-06-18 08:59:50 -0700

    Add <add icon-add-circle-white>

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -108,3 +108,4 @@ export { default as ImageSideService} from './images/1.jpg';
 export { default as ImageSideService2} from './images/2.jpg';
 export { default as ImageSideService3} from './images/3.jpg';
 export { default as ImageSideService4} from './images/4.jpg';
+export { default as IconAddCircleWhite} from './icons/icon-add-circle-outline.svg';
