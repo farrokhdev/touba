50af019c4124028f11a2784aca734dc46a119601
MRezaHeydar1376 <103419484+MRezaHeydar1376@users.noreply.github.com>
2022-06-19 16:27:39 -0700

    Add <add component  CardCalculateProduct for test>

--- src/views/pages/home.tsx
+++ src/views/pages/home.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 import Slider from "react-slick";
 import { BackgroundLatest, IconAddCircle, IconAddCircleWhite, IconAlibaba, IconArrow, IconArrowBlack, IconArvan, IconNext, IconPrev, IconYoutube, ImageControll, ImageLaboratory, ImageNasa, ImagePersonBox, ImageTest, ImageTest2, ImageTest3, ImageTest4 } from "../../assets";
 import { CHART } from "../components/chart";
-import { Button, CardSearchProduct, CardProduct, CardCategory, CardImage, CardCollectionProduct, CardProductWithPrice, CardBestSeller, CardCollectionBest, Divider, CardNewExcess, CardBestBrand, CardNewProduct, CardCalculate, ButtonIcon, CardGroupChat, ButtonIconDashboard, Offcanvas } from "../components/components";
+import { Button, CardSearchProduct, CardProduct, CardCategory, CardImage, CardCollectionProduct, CardProductWithPrice, CardBestSeller, CardCollectionBest, Divider, CardNewExcess, CardBestBrand, CardNewProduct, CardCalculate, ButtonIcon, CardGroupChat, ButtonIconDashboard, Offcanvas, CardCalculateProduct } from "../components/components";
 import HomeController from "../controllers/home_controller";
 import { Pagination, Autoplay, Navigation, Grid } from 'swiper';
 import { Swiper, SwiperSlide } from "swiper/react";
@@ -51,8 +51,10 @@ class Home extends HomeController {
                     />
                 </div>
                 <Offcanvas />
-                <ButtonIconDashboard icon="" dataBsToggle="offcanvas" dataBsTarget="#offcanvasRight" aria-controls="offcanvasRight"/>
-
+                <ButtonIconDashboard icon="" dataBsToggle="offcanvas" dataBsTarget="#offcanvasRight" aria-controls="offcanvasRight" />
+                <div style={{ width: "793px" }}>
+                    <CardCalculateProduct image={ImageTest} name={"Name Product"} price={15} number={10000} />
+                </div>
                 <div className="home-header px-5">
                     <div className="home-header-description">
                         <div className="home-header-description-items">
