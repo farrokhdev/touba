ea0e9bae4b5f496a28ed001445c4f1a0aee16a18
MRezaHeydar1376 <103419484+MRezaHeydar1376@users.noreply.github.com>
2022-07-01 06:12:23 -0700

    Add <add icon_shop>

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -139,4 +139,5 @@ export { default as ImageLadan} from './images/ladan.png';
 export { default as IconNotification} from './icons/icon-notification.svg';
 export { default as IconEmailArrow} from './icons/icon-email-arrow.svg';
 export { default as IconClockSuccess} from './icons/icon-clock-success.svg';
+export { default as IconShop} from './icons/icon-shop.svg';
 export { default as ImageJuicer} from './images/image-juicer.png';
