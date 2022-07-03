866d0a31b1d17cc2a0502aa2f1323a60085babb2
MRezaHeydar1376 <103419484+MRezaHeydar1376@users.noreply.github.com>
2022-07-03 07:14:20 -0700

    Add <add props onActive and add state activeLink to get active link and update styles>

--- src/views/pages/page_test_components.tsx
+++ src/views/pages/page_test_components.tsx
@@ -19,26 +19,22 @@ const TEST_NAV_ITEM = [
         title: "Group Title 1",
         time: "2 MINS",
         notification: 0,
-        location: "/test",
         id: 1
     },
     {
         title: "Group Title 2",
         notification: 10,
         icon: `${IconCalling}`,
-        location: "/",
         id: 2
     },
     {
         title: "Group Title 3",
         time: "2 MINS",
         notification: 0,
-        location: "/",
         id: 3
     },
     {
         title: "Group Title 4",
-        location: "/",
         id: 4
     },
 ];
