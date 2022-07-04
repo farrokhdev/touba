10fefeff82f5c774d15ee82af363ae9ee2c15fde
MRezaHeydar1376 <103419484+MRezaHeydar1376@users.noreply.github.com>
2022-07-04 04:55:26 -0700

    Add <add state image and add function handleGetImage>

--- src/views/controllers/company_profile_controller.ts
+++ src/views/controllers/company_profile_controller.ts
@@ -1,21 +1,63 @@
-import { Component } from "react";
-import { IconCalling } from "../../assets";
+import { ChangeEvent, Component } from "react";
+import { IconCalling, IconCallSorme, IconLocationSorme, IconPaperBlack, IconStarSorme, IconWalletSorme } from "../../assets";
 
 interface IProps { }
 interface IState {
     activeLink: number;
-
+    image: File | string;
 }
+
+
+export const TEST_NAV_ITEM = [
+    {
+        title: "About Page",
+        icon: `${IconPaperBlack}`,
+        id: 1
+    },
+    {
+        title: "Contact Us Page",
+        icon: `${IconCallSorme}`,
+        id: 2
+    },
+    {
+        title: "Payments Setting",
+        icon: `${IconWalletSorme}`,
+        id: 3
+    },
+    {
+        title: "Payments Setting",
+        icon: `${IconLocationSorme}`,
+        id: 4
+    },
+    {
+        title: "Visual Settings",
+        icon: `${IconStarSorme}`,
+        id: 5
+    }
+];
 
 export class CompanyProfileController extends Component<IProps, IState> {
    
     state = {
         activeLink: 1,
+        image: '',
     };
 
     handleGetActiveLink = (id: number) => {
         this.setState({activeLink: id})
     }
+
+    handleGetImage = (event: ChangeEvent<HTMLInputElement>) => {
+        const files = (event.currentTarget as HTMLInputElement).files;
+        if (files && files.length > 0) {
+            this.setState({ image:  URL.createObjectURL(files[0]) });
+        }
+        console.log(this.state.image);
+    };
+    
+    // handleDeleteImage = (name: any) => {
+    //     this.setState({ image: this.state.images.filter(image => image !== name) })
+    // }
     
 }
 export default CompanyProfileController;
