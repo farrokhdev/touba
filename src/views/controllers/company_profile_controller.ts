7acca49d99ee1f963f601a8d8c300f8210bf5235
MRezaHeydar1376 <103419484+MRezaHeydar1376@users.noreply.github.com>
2022-07-04 13:33:49 -0700

    Add <add component modal and add function in controller to get values and add styles>

--- src/views/controllers/company_profile_controller.ts
+++ src/views/controllers/company_profile_controller.ts
@@ -5,6 +5,11 @@ interface IProps { }
 interface IState {
     activeLink: number;
     image: File | string;
+    activeTab: number;
+    modalDelete: boolean;
+    modalDescription: boolean;
+    getDiscription: string;
+    description: string;
 }
 
 
@@ -37,27 +42,63 @@ export const TEST_NAV_ITEM = [
 ];
 
 export class CompanyProfileController extends Component<IProps, IState> {
-   
+
     state = {
         activeLink: 1,
         image: '',
+        activeTab: 1,
+        modalDelete: false,
+        modalDescription: false,
+        getDiscription: "",
+        description: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi",
     };
 
     handleGetActiveLink = (id: number) => {
-        this.setState({activeLink: id})
+        this.setState({ activeLink: id })
     }
 
     handleGetImage = (event: ChangeEvent<HTMLInputElement>) => {
         const files = (event.currentTarget as HTMLInputElement).files;
         if (files && files.length > 0) {
-            this.setState({ image:  URL.createObjectURL(files[0]) });
+            this.setState({ image: URL.createObjectURL(files[0]) });
         }
         console.log(this.state.image);
     };
-    
+
     handleDeleteImage = () => {
+        this.setState({ modalDelete: true })
         this.setState({ image: "" })
+    };
+
+    handleGetActiveTab = (id: number) => {
+        this.setState({ activeTab: id })
+    };
+
+    handleShowModalDelete = () => {
+        this.setState({ modalDelete: true })
+    };
+
+    handleCloseModalDelete = () => {
+        this.setState({ modalDelete: false })
+    };
+
+    handleShowModalDescription = () => {
+        this.setState({ modalDescription: true })
+    };
+
+    handleCloseModalDescription = () => {
+        this.setState({ modalDescription: false })
+    };
+
+    handleSubmitChangeDescription = () => {
+        this.setState({description: this.state.getDiscription});
+        this.setState({ modalDescription: false });
+    };
+    
+    handleGetDescription = (text: string) => {
+        this.setState({getDiscription: text})
     }
     
+
 }
 export default CompanyProfileController;
--- src/views/pages/company_profile_edit/index.tsx
+++ src/views/pages/company_profile_edit/index.tsx
@@ -1,11 +1,14 @@
 import { IconCalling, IconCallSorme, IconDeletGreen, IconEdit, IconEditSorme, IconImageBlack, IconLocationSorme, IconPaperBlack, IconStarSorme, IconWalletSorme, ImageBanner } from "../../../assets";
+import { ModalCompany, TextAreaLabel } from "../../components/components";
 import DashboardNav from "../../components/dashboard_nav";
+import DeleteImage from "../../components/delete_image";
+import MyCompanyNav from "../../components/mycompany_nav";
 import { SideNavItems } from "../../components/side_nav_items";
 import CompanyProfileController, { TEST_NAV_ITEM } from "../../controllers/company_profile_controller";
 
 class CompanyProfileEdit extends CompanyProfileController {
     render() {
-
+        const { image, modalDelete, modalDescription, description } = this.state;
         return (
             <div className="company-profile-edit">
                 <DashboardNav title={"My Company Profile"} />
@@ -15,7 +18,7 @@ class CompanyProfileEdit extends CompanyProfileController {
                     </div>
                     <div className="body-content">
                         <div className="body-content-header">
-
+                            <MyCompanyNav onActive={this.handleGetActiveTab} />
                         </div>
                         <div className="body-content-items">
                             <div className="body-content-items-title">
@@ -39,41 +42,237 @@ class CompanyProfileEdit extends CompanyProfileController {
                                     <img src={IconDeletGreen} alt="" onClick={this.handleDeleteImage} />
                                 </div>
                                 <div className="body-content-items-banner-image">
-                                    <img src={this.state.image === "" ? ImageBanner : this.state.image} alt="" />
+                                    <img
+                                        src={image === "" ? ImageBanner : image}
+                                        alt=""
+                                    />
                                 </div>
                             </div>
                             <div className="body-content-items-description">
                                 <div className="body-content-items-description-icon">
-                                    <img src={IconEditSorme} alt="" />
+                                    <img src={IconEditSorme} alt="" onClick={this.handleShowModalDescription} />
                                 </div>
                                 <div className="body-content-items-description-text">
                                     <p>
-                                        Lorem ipsum dolor sit amet,
-                                        consectetuer adipiscing elit,
-                                        sed diam nonummy nibh euismod
-                                        tincidunt ut laoreet dolore magna
-                                        aliquam erat volutpat. Ut wisi enim
-                                        ad minim veniam, quis nostrud exerci t
-                                        ation ullamcorper suscipit lobortis ni
-                                        sl ut aliquip ex ea commodo consequat.
-                                        Duis autem vel eum iriure dolor in he
-
-                                        ndrerit in vulputate velit esse moles
-
-                                        tie consequat, vel illum dolore eu fe
-                                        ugiat nulla facilisis at vero eros et
-                                        accumsan et iusto odio dignissim qui
-
-                                        blandit praesent luptatum zzril del
-                                        enit augue duis dolore te feugait n
-                                        ulla facilisi.
-
-                                        Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.
+                                        {description}
                                     </p>
 
                                 </div>
                             </div>
+                            <div className="body-content-items-details">
+                                <div className="body-content-items-details-icon">
+                                    <img src={IconEditSorme} alt="" />
+                                </div>
+                                <div className="body-content-items-details-table">
+                                    <div className="body-content-items-details-table-row">
+                                        <div className="body-content-items-details-table-row-title">
+                                            <h6>
+                                                Established Year
+                                            </h6>
+                                        </div>
+                                        <div className="body-content-items-details-table-row-amount">
+                                            <h6>
+                                                2015
+                                            </h6>
+                                        </div>
+                                    </div>
+                                    <div className="body-content-items-details-table-row">
+                                        <div className="body-content-items-details-table-row-title">
+                                            <h6>
+                                                Business Type:
+                                            </h6>
+                                        </div>
+                                        <div className="body-content-items-details-table-row-amount">
+                                            <h6>
+                                                Manufacturer/Factory
+                                            </h6>
+                                        </div>
+                                    </div>
+                                    <div className="body-content-items-details-table-row">
+                                        <div className="body-content-items-details-table-row-title">
+                                            <h6>
+                                                Business Category
+                                            </h6>
+                                        </div>
+                                        <div className="body-content-items-details-table-row-amount">
+                                            <h6>
+                                                Cosmetic and Hygiene
+                                            </h6>
+                                        </div>
+                                    </div>
+                                    <div className="body-content-items-details-table-row">
+                                        <div className="body-content-items-details-table-row-title">
+                                            <h6>
+                                                Main Products:
+                                            </h6>
+                                        </div>
+                                        <div className="body-content-items-details-table-row-amount text-red">
+                                            <h6>
+                                                Milk Powder , Food Powders , Food raw materials
+                                            </h6>
+                                        </div>
+                                    </div>
+                                    <div className="body-content-items-details-table-row">
+                                        <div className="body-content-items-details-table-row-title">
+                                            <h6>
+                                                Main Products:
+                                            </h6>
+                                        </div>
+                                        <div className="body-content-items-details-table-row-amount text-red">
+                                            <h6>
+                                                Wheel Loader , Forklift , Excavator , Tractor , Bulldozer , Roadroller , Trailer , Telescopic ...
+                                            </h6>
+                                        </div>
+                                    </div>
+                                    <div className="body-content-items-details-table-row">
+                                        <div className="body-content-items-details-table-row-title">
+                                            <h6>
+                                                Main Market:
+                                            </h6>
+                                        </div>
+                                        <div className="body-content-items-details-table-row-amount text-red">
+                                            <h6>
+                                                Iran , Iraq , Turkey
+                                            </h6>
+                                        </div>
+                                    </div>
+                                    <div className="body-content-items-details-table-row">
+                                        <div className="body-content-items-details-table-row-title">
+                                            <h6>
+                                                Number of empolyee
+                                            </h6>
+                                        </div>
+                                        <div className="body-content-items-details-table-row-amount">
+                                            <h6>
+                                                Number of empolyee
+                                            </h6>
+                                        </div>
+                                    </div>
+                                    <div className="body-content-items-details-table-row">
+                                        <div className="body-content-items-details-table-row-title">
+                                            <h6>
+                                                Registered Capital:
+                                            </h6>
+                                        </div>
+                                        <div className="body-content-items-details-table-row-amount">
+                                            <h6>
+                                                2000000 RMB
+                                            </h6>
+                                        </div>
+                                    </div>
+                                    <div className="body-content-items-details-table-row">
+                                        <div className="body-content-items-details-table-row-title">
+                                            <h6>
+                                                Plant Area:
+                                            </h6>
+                                        </div>
+                                        <div className="body-content-items-details-table-row-amount">
+                                            <h6>
+                                                {">"}2000 square meters
+                                            </h6>
+                                        </div>
+                                    </div>
+                                    <div className="body-content-items-details-table-row">
+                                        <div className="body-content-items-details-table-row-title">
+                                            <h6>
+                                                Factory Size:
+                                            </h6>
+                                        </div>
+                                        <div className="body-content-items-details-table-row-amount">
+                                            <h6>
+                                                Size
+                                            </h6>
+                                        </div>
+                                    </div>
+                                    <div className="body-content-items-details-table-row">
+                                        <div className="body-content-items-details-table-row-title">
+                                            <h6>
+                                                Payment Methode:
+                                            </h6>
+                                        </div>
+                                        <div className="body-content-items-details-table-row-amount">
+                                            <h6>
+                                                Paypal, Cash
+                                            </h6>
+                                        </div>
+                                    </div>
+                                    <div className="body-content-items-details-table-row">
+                                        <div className="body-content-items-details-table-row-title">
+                                            <h6>
+                                                Management System Certification:
+                                            </h6>
+                                        </div>
+                                        <div className="body-content-items-details-table-row-amount">
+                                            <h6>
+                                                ISO 9001, ISO 9000, ISO 14001, ISO 14000, ISO 20000, OHSAS/ OHSMS 18001, IATF16949, HSE, ...
+                                            </h6>
+                                        </div>
+                                    </div>
+                                    <div className="body-content-items-details-table-row">
+                                        <div className="body-content-items-details-table-row-title">
+                                            <h6>
+                                                Average Lead Time:
+                                            </h6>
+                                        </div>
+                                        <div className="body-content-items-details-table-row-amount">
+                                            <h6>
+                                                Peak Season Lead Time: one month, Off Season Lead Time: within 15 workdays
+                                            </h6>
+                                        </div>
+                                    </div>
+                                    <div className="body-content-items-details-table-row">
+                                        <div className="body-content-items-details-table-row-title">
+                                            <h6>
+                                                SGS Serial NO. :
+                                            </h6>
+                                        </div>
+                                        <div className="body-content-items-details-table-row-amount">
+                                            <h6>
+                                                QIP-ASI185372
+                                                <span>
+                                                    Report Verification
+                                                </span>
+                                            </h6>
+                                        </div>
+                                    </div>
+                                    <div className="body-content-items-details-table-row">
+                                        <div className="body-content-items-details-table-row-title">
+                                            <h6>
+                                                Average Lead Time:
+                                            </h6>
+                                        </div>
+                                        <div className="body-content-items-details-table-row-amount">
+                                            <h6>
+                                                Peak Season Lead Time: one month, Off Season Lead Time: within 15 workdays
+                                            </h6>
+                                        </div>
+                                    </div>
+                                    <div className="body-content-items-details-table-row">
+                                        <div className="body-content-items-details-table-row-title">
+                                            <h6>
+                                                SGS Serial NO. :
+                                            </h6>
+                                        </div>
+                                        <div className="body-content-items-details-table-row-amount">
+                                            <h6>
+                                                QIP-ASI185372
+                                                <span>
+                                                    Report Verification
+                                                </span>
+                                            </h6>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
+                        {modalDelete === true ? (
+                            <DeleteImage onClick={this.handleCloseModalDelete} />
+                        ) : (null)}
+                        {modalDescription === true ? (
+                            <ModalCompany onClose={this.handleCloseModalDescription} onSave={this.handleSubmitChangeDescription}>
+                                <TextAreaLabel title={"Introduction"} handleGetValue={this.handleGetDescription} />
+                            </ModalCompany>
+                        ) : (null)}
                     </div>
                 </div>
             </div>
