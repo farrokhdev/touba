0feb30626dbc0fe2d65e608cd151b6d7e8228be6
MRezaHeydar1376 <103419484+MRezaHeydar1376@users.noreply.github.com>
2022-07-04 05:12:08 -0700

    Add <add section description and add styles>

--- src/views/pages/company_profile_edit/index.tsx
+++ src/views/pages/company_profile_edit/index.tsx
@@ -1,4 +1,4 @@
-import { IconCalling, IconCallSorme, IconDeletGreen, IconImageBlack, IconLocationSorme, IconPaperBlack, IconStarSorme, IconWalletSorme, ImageBanner } from "../../../assets";
+import { IconCalling, IconCallSorme, IconDeletGreen, IconEdit, IconEditSorme, IconImageBlack, IconLocationSorme, IconPaperBlack, IconStarSorme, IconWalletSorme, ImageBanner } from "../../../assets";
 import DashboardNav from "../../components/dashboard_nav";
 import { SideNavItems } from "../../components/side_nav_items";
 import CompanyProfileController, { TEST_NAV_ITEM } from "../../controllers/company_profile_controller";
@@ -27,19 +27,50 @@ class CompanyProfileEdit extends CompanyProfileController {
                                 <div className="body-content-items-banner-icons">
                                     <div className="input-file">
                                         <label className="label-photo">
-                                        <img src={IconImageBlack} alt="icon-image" className="icon-image" />
+                                            <img src={IconImageBlack} alt="icon-image" className="icon-image" />
                                         </label>
                                         <input
                                             accept="image/png, image/jpg, image/jpeg"
-                                            type="file" 
+                                            type="file"
                                             className="input-photo"
                                             onChange={(event) => this.handleGetImage(event)}
                                         />
                                     </div>
-                                    <img src={IconDeletGreen} alt="" onClick={this.handleDeleteImage}/>
+                                    <img src={IconDeletGreen} alt="" onClick={this.handleDeleteImage} />
                                 </div>
                                 <div className="body-content-items-banner-image">
                                     <img src={this.state.image === "" ? ImageBanner : this.state.image} alt="" />
+                                </div>
+                            </div>
+                            <div className="body-content-items-description">
+                                <div className="body-content-items-description-icon">
+                                    <img src={IconEditSorme} alt="" />
+                                </div>
+                                <div className="body-content-items-description-text">
+                                    <p>
+                                        Lorem ipsum dolor sit amet,
+                                        consectetuer adipiscing elit,
+                                        sed diam nonummy nibh euismod
+                                        tincidunt ut laoreet dolore magna
+                                        aliquam erat volutpat. Ut wisi enim
+                                        ad minim veniam, quis nostrud exerci t
+                                        ation ullamcorper suscipit lobortis ni
+                                        sl ut aliquip ex ea commodo consequat.
+                                        Duis autem vel eum iriure dolor in he
+
+                                        ndrerit in vulputate velit esse moles
+
+                                        tie consequat, vel illum dolore eu fe
+                                        ugiat nulla facilisis at vero eros et
+                                        accumsan et iusto odio dignissim qui
+
+                                        blandit praesent luptatum zzril del
+                                        enit augue duis dolore te feugait n
+                                        ulla facilisi.
+
+                                        Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.
+                                    </p>
+
                                 </div>
                             </div>
                         </div>
