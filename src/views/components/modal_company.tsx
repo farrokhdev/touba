b203d2d92221565a8eee50d24b6bff8849385b81
MRezaHeydar1376 <103419484+MRezaHeydar1376@users.noreply.github.com>
2022-07-04 13:32:50 -0700

    Add <add prop children>

--- src/views/components/modal_company.tsx
+++ src/views/components/modal_company.tsx
@@ -2,56 +2,53 @@ import {
     memo,
     ReactElement,
     useState,
-    MouseEvent
+    MouseEvent,
+    HTMLAttributes
 } from "react";
 import { IconClose } from "../../assets";
 import { Divider } from "./components";
 
-interface Props {
-    show: boolean;
+interface Props extends HTMLAttributes<HTMLElement>{
     onClose(): void;
+    onSave(): void;
 }
 
-export function ModalCompany({ show, onClose }: Props): ReactElement {
+export function ModalCompany({ onClose, children, onSave }: Props): ReactElement {
 
-    function handlePreventOnClick (event: MouseEvent<HTMLElement>) {
+    function handlePreventOnClick(event: MouseEvent<HTMLElement>) {
         event.stopPropagation();
     }
 
     return (
-        <>
-            {show === true  ? (
-                <div className="modal-company" onClick={onClose}>
-                    <div className="modal-company-card" onClick={handlePreventOnClick}>
-                        <div className="modal-company-card-header">
-                            <h4 className="modal-company-card-header-title">
-                                Company Introduction
-                            </h4>
-                            <span className="modal-company-card-header-close" onClick={onClose}>
-                                <img src={IconClose} alt="" />
-                            </span>
-                        </div>
-                        <Divider direction={"vertical"} />
-                        <div className="modal-company-card-content">
-
-                        </div>
-                        <Divider direction={"vertical"} />
-                        <div className="modal-company-card-footer">
-                            <button className="modal-company-card-footer-button-save">
-                                <h5 className="modal-company-card-footer-button-save-text">
-                                    SAVE CHANGES
-                                </h5>
-                            </button>
-                            <button className="modal-company-card-footer-button-cancel"  onClick={onClose}>
-                                <h5 className="modal-company-card-footer-button-cancel-text">
-                                    CANCEL
-                                </h5>
-                            </button>
-                        </div>
-                    </div>
+        <div className="modal-company" onClick={onClose}>
+            <div className="modal-company-card" onClick={handlePreventOnClick}>
+                <div className="modal-company-card-header">
+                    <h4 className="modal-company-card-header-title">
+                        Company Introduction
+                    </h4>
+                    <span className="modal-company-card-header-close" onClick={onClose}>
+                        <img src={IconClose} alt="" />
+                    </span>
                 </div>
-            ) : (null)}
-        </>
+                <Divider direction={"vertical"} />
+                <div className="modal-company-card-content">
+                    {children}
+                </div>
+                <Divider direction={"vertical"} />
+                <div className="modal-company-card-footer">
+                    <button className="modal-company-card-footer-button-save" onClick={onSave}>
+                        <h5 className="modal-company-card-footer-button-save-text">
+                            SAVE CHANGES
+                        </h5>
+                    </button>
+                    <button className="modal-company-card-footer-button-cancel" onClick={onClose}>
+                        <h5 className="modal-company-card-footer-button-cancel-text">
+                            CANCEL
+                        </h5>
+                    </button>
+                </div>
+            </div>
+        </div>
     )
 }
 
