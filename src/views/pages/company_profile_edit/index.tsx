2bfe242fdd5df4ef38af78fe59e43981d363adbf
MRezaHeydar1376 <103419484+MRezaHeydar1376@users.noreply.github.com>
2022-07-04 04:55:46 -0700

    Add <create section banner>

--- src/views/pages/company_profile_edit/index.tsx
+++ src/views/pages/company_profile_edit/index.tsx
@@ -1,44 +1,49 @@
-import { IconCalling, IconCallSorme, IconLocationSorme, IconPaperBlack, IconStarSorme, IconWalletSorme } from "../../../assets";
+import { IconCalling, IconCallSorme, IconDeletGreen, IconImageBlack, IconLocationSorme, IconPaperBlack, IconStarSorme, IconWalletSorme, ImageBanner } from "../../../assets";
 import DashboardNav from "../../components/dashboard_nav";
 import { SideNavItems } from "../../components/side_nav_items";
-import CompanyProfileController from "../../controllers/company_profile_controller";
-
-const TEST_NAV_ITEM = [
-    {
-        title: "About Page",
-        icon: `${IconPaperBlack}`,
-        id: 1
-    },
-    {
-        title: "Contact Us Page",
-        icon: `${IconCallSorme}`,
-        id: 2
-    },
-    {
-        title: "Payments Setting",
-        icon: `${IconWalletSorme}`,
-        id: 3
-    },
-    {
-        title: "Payments Setting",
-        icon: `${IconLocationSorme}`,
-        id: 4
-    },
-    {
-        title: "Visual Settings",
-        icon: `${IconStarSorme}`,
-        id: 5
-    }
-];
+import CompanyProfileController, { TEST_NAV_ITEM } from "../../controllers/company_profile_controller";
 
 class CompanyProfileEdit extends CompanyProfileController {
     render() {
-        
+
         return (
             <div className="company-profile-edit">
                 <DashboardNav title={"My Company Profile"} />
-                <div className="company-profile-edit-side-item">
-                    <SideNavItems items={TEST_NAV_ITEM} onActive={this.handleGetActiveLink}/>
+                <div className="body">
+                    <div className="body-side-item">
+                        <SideNavItems items={TEST_NAV_ITEM} onActive={this.handleGetActiveLink} />
+                    </div>
+                    <div className="body-content">
+                        <div className="body-content-header">
+
+                        </div>
+                        <div className="body-content-items">
+                            <div className="body-content-items-title">
+                                <h4 className="body-content-items-title-text">
+                                    Company Profile
+                                </h4>
+                            </div>
+                            <div className="body-content-items-banner">
+                                <div className="body-content-items-banner-icons">
+                                    <div className="input-file">
+                                        <label className="label-photo">
+                                        <img src={IconImageBlack} alt="icon-image" className="icon-image" />
+                                        </label>
+                                        <input
+                                            accept="image/png, image/jpg, image/jpeg"
+                                            type="file" 
+                                            className="input-photo"
+                                            onChange={(event) => this.handleGetImage(event)}
+                                        />
+                                    </div>
+                                    <img src={IconDeletGreen} alt="" />
+                                </div>
+                                <div className="body-content-items-banner-image">
+                                    <img src={this.state.image === "" ? ImageBanner : this.state.image} alt="" />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         )
