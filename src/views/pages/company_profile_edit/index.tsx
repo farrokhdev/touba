a462adf0e004c571f07cceca573b7e214bcb96ea
MRezaHeydar1376 <103419484+MRezaHeydar1376@users.noreply.github.com>
2022-07-05 04:08:58 -0700

    Add <create sectionSpacifications>

--- src/views/pages/company_profile_edit/index.tsx
+++ src/views/pages/company_profile_edit/index.tsx
@@ -17,15 +17,7 @@ class CompanyProfileEdit extends CompanyProfileController {
     sectionDescription() {
         const {
             image,
-            modalDelete,
-            modalDescription,
             description,
-            modalDetails,
-            establishedYear,
-            mainProducts,
-            mainMarket,
-            numberOfEmployee,
-            registeredCapital,
             details
         } = this.state;
         return (
@@ -279,6 +271,229 @@ class CompanyProfileEdit extends CompanyProfileController {
             </>
         )
     }
+    sectionSpecifications() {
+        const {
+            details
+        } = this.state;
+        return (
+            <>
+                <div className="body-content-items-title">
+                    <h4 className="body-content-items-title-text">
+                        Specifications
+                    </h4>
+                </div>
+                <div className="body-content-items-details">
+                    <div className="body-content-items-details-icon">
+                        <img
+                            src={IconEditSorme}
+                            alt=""
+                            onClick={this.handleShowModalDetails}
+                        />
+                    </div>
+                    <div className="body-content-items-details-table">
+                        <div className="body-content-items-details-table-row">
+                            <div className="body-content-items-details-table-row-title">
+                                <h6>
+                                    Established Year
+                                </h6>
+                            </div>
+                            <div className="body-content-items-details-table-row-amount">
+                                <h6>
+                                    {details.establishedYear}
+                                </h6>
+                            </div>
+                        </div>
+                        <div className="body-content-items-details-table-row">
+                            <div className="body-content-items-details-table-row-title">
+                                <h6>
+                                    Business Type:
+                                </h6>
+                            </div>
+                            <div className="body-content-items-details-table-row-amount">
+                                <h6>
+                                    {details.businessType}
+                                </h6>
+                            </div>
+                        </div>
+                        <div className="body-content-items-details-table-row">
+                            <div className="body-content-items-details-table-row-title">
+                                <h6>
+                                    Business Category
+                                </h6>
+                            </div>
+                            <div className="body-content-items-details-table-row-amount">
+                                <h6>
+                                    {details.businessCategory}
+                                </h6>
+                            </div>
+                        </div>
+                        <div className="body-content-items-details-table-row">
+                            <div className="body-content-items-details-table-row-title">
+                                <h6>
+                                    Main Products:
+                                </h6>
+                            </div>
+                            <div className="body-content-items-details-table-row-amount text-red">
+                                <h6>
+                                    {details.mainProducts}
+                                </h6>
+                            </div>
+                        </div>
+                        <div className="body-content-items-details-table-row">
+                            <div className="body-content-items-details-table-row-title">
+                                <h6>
+                                    Main Products:
+                                </h6>
+                            </div>
+                            <div className="body-content-items-details-table-row-amount text-red">
+                                <h6>
+                                    {details.mainProducts}
+                                </h6>
+                            </div>
+                        </div>
+                        <div className="body-content-items-details-table-row">
+                            <div className="body-content-items-details-table-row-title">
+                                <h6>
+                                    Main Market:
+                                </h6>
+                            </div>
+                            <div className="body-content-items-details-table-row-amount text-red">
+                                <h6>
+                                    {details.mainMarket}
+                                </h6>
+                            </div>
+                        </div>
+                        <div className="body-content-items-details-table-row">
+                            <div className="body-content-items-details-table-row-title">
+                                <h6>
+                                    Number of empolyee
+                                </h6>
+                            </div>
+                            <div className="body-content-items-details-table-row-amount">
+                                <h6>
+                                    {details.numberOfEmployee}
+                                </h6>
+                            </div>
+                        </div>
+                        <div className="body-content-items-details-table-row">
+                            <div className="body-content-items-details-table-row-title">
+                                <h6>
+                                    Registered Capital:
+                                </h6>
+                            </div>
+                            <div className="body-content-items-details-table-row-amount">
+                                <h6>
+                                    {details.registeredCapital}
+                                </h6>
+                            </div>
+                        </div>
+                        <div className="body-content-items-details-table-row">
+                            <div className="body-content-items-details-table-row-title">
+                                <h6>
+                                    Plant Area:
+                                </h6>
+                            </div>
+                            <div className="body-content-items-details-table-row-amount">
+                                <h6>
+                                    {details.planeArea}
+                                </h6>
+                            </div>
+                        </div>
+                        <div className="body-content-items-details-table-row">
+                            <div className="body-content-items-details-table-row-title">
+                                <h6>
+                                    Factory Size:
+                                </h6>
+                            </div>
+                            <div className="body-content-items-details-table-row-amount">
+                                <h6>
+                                    Size
+                                </h6>
+                            </div>
+                        </div>
+                        <div className="body-content-items-details-table-row">
+                            <div className="body-content-items-details-table-row-title">
+                                <h6>
+                                    Payment Methode:
+                                </h6>
+                            </div>
+                            <div className="body-content-items-details-table-row-amount">
+                                <h6>
+                                    Paypal, Cash
+                                </h6>
+                            </div>
+                        </div>
+                        <div className="body-content-items-details-table-row">
+                            <div className="body-content-items-details-table-row-title">
+                                <h6>
+                                    Management System Certification:
+                                </h6>
+                            </div>
+                            <div className="body-content-items-details-table-row-amount">
+                                <h6>
+                                    ISO 9001, ISO 9000, ISO 14001, ISO 14000, ISO 20000, OHSAS/ OHSMS 18001, IATF16949, HSE, ...
+                                </h6>
+                            </div>
+                        </div>
+                        <div className="body-content-items-details-table-row">
+                            <div className="body-content-items-details-table-row-title">
+                                <h6>
+                                    Average Lead Time:
+                                </h6>
+                            </div>
+                            <div className="body-content-items-details-table-row-amount">
+                                <h6>
+                                    Peak Season Lead Time: one month, Off Season Lead Time: within 15 workdays
+                                </h6>
+                            </div>
+                        </div>
+                        <div className="body-content-items-details-table-row">
+                            <div className="body-content-items-details-table-row-title">
+                                <h6>
+                                    SGS Serial NO. :
+                                </h6>
+                            </div>
+                            <div className="body-content-items-details-table-row-amount">
+                                <h6>
+                                    QIP-ASI185372
+                                    <span>
+                                        Report Verification
+                                    </span>
+                                </h6>
+                            </div>
+                        </div>
+                        <div className="body-content-items-details-table-row">
+                            <div className="body-content-items-details-table-row-title">
+                                <h6>
+                                    Average Lead Time:
+                                </h6>
+                            </div>
+                            <div className="body-content-items-details-table-row-amount">
+                                <h6>
+                                    Peak Season Lead Time: one month, Off Season Lead Time: within 15 workdays
+                                </h6>
+                            </div>
+                        </div>
+                        <div className="body-content-items-details-table-row">
+                            <div className="body-content-items-details-table-row-title">
+                                <h6>
+                                    SGS Serial NO. :
+                                </h6>
+                            </div>
+                            <div className="body-content-items-details-table-row-amount">
+                                <h6>
+                                    QIP-ASI185372
+                                    <span>
+                                        Report Verification
+                                    </span>
+                                </h6>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </>
+        )
+    }
     render() {
         const {
             image,
@@ -310,7 +525,14 @@ class CompanyProfileEdit extends CompanyProfileController {
                             {activeTab === 1 ? (
                                 this.sectionDescription()
                             ) : (
-                                <h3>dfgd</h3>
+                                activeTab === 2 ? (
+                                    this.sectionSpecifications()
+                                ) : (
+                                    <h3>
+                                        77
+                                        87
+                                    </h3>
+                                )
                             )}
                         </div>
                         {modalDelete === true ? (
