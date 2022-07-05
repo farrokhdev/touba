948a9f8d356c59ee215c9cca35c9c332a070a065
MRezaHeydar1376 <103419484+MRezaHeydar1376@users.noreply.github.com>
2022-07-05 10:55:24 -0700

    Add <create component CardUploadImage and add styles>

---
+++ src/views/components/card_upload_image.tsx
@@ -0,0 +1,74 @@
+import { ChangeEvent, memo, ReactElement, useCallback, useState } from "react";
+import { IconDeletGreen, IconImage, IconUpload } from "../../assets";
+
+
+interface Props {
+    title: string;
+    handleGetImage(image: string): void;
+}
+
+export function CardUploadImage({ handleGetImage }: Props): ReactElement {
+
+    const [image, setImage] = useState("");
+
+    const handleGetImageCard = useCallback((event: ChangeEvent<HTMLInputElement>) => {
+        const files = (event.currentTarget as HTMLInputElement).files;
+        if (files && files.length > 0) {
+            setImage(URL.createObjectURL(files[0]))
+            handleGetImage(image);
+        }
+    },  [image]);
+
+    function handleDeleteImage() {
+        setImage("");
+    }
+
+    return (
+        <>
+            {image === "" ? (
+                <div className="card-upload-image">
+                    <div className="card-upload-image-content">
+                        <div className="card-upload-image-content-input">
+                            <img
+                                src={IconImage}
+                                alt=""
+                            />
+                            <input
+                                type="file"
+                                onChange={handleGetImageCard}
+                            />
+                        </div>
+                        <h5 className="card-upload-image-content-text">
+                            Upload Certificate Image Here
+                        </h5>
+                    </div>
+                </div>
+            ) : (
+                <div className="card-show-image">
+                    <img
+                        src={image}
+                        alt=""
+                    />
+                    <div className="card-show-image-buttons">
+                        <div className="card-show-image-buttons-upload">
+                            <img
+                                src={IconImage}
+                                alt=""
+                            />
+                            <input
+                                type="file"
+                                onChange={handleGetImageCard}
+                            />
+                        </div>
+                        <button onClick={handleDeleteImage}>
+                            <img src={IconDeletGreen} alt="" />
+                        </button>
+                    </div>
+                </div>
+            )}
+        </>
+
+    )
+}
+
+export default memo(CardUploadImage);
--- src/views/components/components.ts
+++ src/views/components/components.ts
@@ -94,6 +94,8 @@ export { default as ProductCart } from './product_cart';
 export { default as Location } from './location';
 export { default as ModalCompany } from './modal_company';
 export { default as TextAreaLabel } from './textarea_lable';
+export { default as InputDate } from './input_date';
+export { default as CardUploadImage } from './card_upload_image';
 
 
 
