27fce6db513f83f0fc14ff5b1ae6bc8c68e9fdd1
MRezaHeydar1376 <103419484+MRezaHeydar1376@users.noreply.github.com>
2022-07-09 02:34:13 -0700

    Add <add architect to folder report>

--- src/views/pages/report/index.tsx
+++ src/views/pages/report/index.tsx
@@ -5,8 +5,12 @@ import { ButtonOutline, ChartDoughnut, Divider, Summary, TextIconVertical } from
 import ComponentLineChart from "../../components/component_line_chart";
 import DashboardNav from "../../components/dashboard_nav";
 import ReportController from "../../controllers/report_controller";
+import Sales from "./sales";
+import Statistics from "./statistics";
+import SummaryPage from "./summary";
 
 class Report extends ReportController {
+
     render() {
         return (
             <div className="report">
@@ -39,221 +43,18 @@ class Report extends ReportController {
                     />
                 </div>
                 <div className="report-sales">
-                    <div className="report-sales-countries">
-                        <div className="report-sales-countries-map">
-                            <h4>
-                                Most Sales in Countries
-                            </h4>
-                            <img src={ImageWorld} alt="" />
-                        </div>
-                        <div className="report-sales-countries-divider">
-                            <Divider direction={"horizontal"} />
-                        </div>
-                        <div className="report-sales-countries-flag">
-                            <h6>
-                                Top Countries
-                            </h6>
-                            <div className="report-sales-countries-flag-top">
-                                <div className="report-sales-countries-flag-top-name">
-                                    <img src={Iran} alt="" />
-                                    <h6>
-                                        Canada
-                                    </h6>
-                                </div>
-                                <h5 className="report-sales-countries-flag-top-sale">
-                                    <span>$</span>
-                                    152,423
-                                </h5>
-                            </div>
-                            <div className="report-sales-countries-flag-top">
-                                <div className="report-sales-countries-flag-top-name">
-                                    <img src={Iran} alt="" />
-                                    <h6>
-                                        Canada
-                                    </h6>
-                                </div>
-                                <h5 className="report-sales-countries-flag-top-sale">
-                                    <span>$</span>
-                                    152,423
-                                </h5>
-                            </div>
-                            <div className="report-sales-countries-flag-top">
-                                <div className="report-sales-countries-flag-top-name">
-                                    <img src={Iran} alt="" />
-                                    <h6>
-                                        Canada
-                                    </h6>
-                                </div>
-                                <h5 className="report-sales-countries-flag-top-sale">
-                                    <span>$</span>
-                                    152,423
-                                </h5>
-                            </div>
-                            <div className="report-sales-countries-flag-top">
-                                <div className="report-sales-countries-flag-top-name">
-                                    <img src={Iran} alt="" />
-                                    <h6>
-                                        Canada
-                                    </h6>
-                                </div>
-                                <h5 className="report-sales-countries-flag-top-sale">
-                                    <span>$</span>
-                                    152,423
-                                </h5>
-                            </div>
-                            <div className="report-sales-countries-flag-top">
-                                <div className="report-sales-countries-flag-top-name">
-                                    <img src={Iran} alt="" />
-                                    <h6>
-                                        Canada
-                                    </h6>
-                                </div>
-                                <h5 className="report-sales-countries-flag-top-sale">
-                                    <span>$</span>
-                                    152,423
-                                </h5>
-                            </div>
-                            <div className="report-sales-countries-flag-top">
-                                <div className="report-sales-countries-flag-top-name">
-                                    <img src={Iran} alt="" />
-                                    <h6>
-                                        Canada
-                                    </h6>
-                                </div>
-                                <h5 className="report-sales-countries-flag-top-sale">
-                                    <span>$</span>
-                                    152,423
-                                </h5>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="report-sales-download">
-                        <div className="report-sales-download-report">
-                            <h5 className="report-sales-download-report-title">
-                                Download Reports
-                            </h5>
-                            <p className="report-sales-download-report-description">
-                                Download Touba’s last year sales and marketing digest. It can help you to make the right decisions
-                            </p>
-                            <button className="report-sales-download-report-button">
-                                <img src={IconDownload} alt="" />
-                                <h5>
-                                    TOUBA`S ANNUAL REPORT
-                                </h5>
-                            </button>
-                        </div>
-                        <div className="report-sales-download-summary">
-                            <p className="report-sales-download-summary-description">
-                                Download your annual sell/buy summary here. You can analaysis your products here.
-                            </p>
-                            <button className="report-sales-download-summary-button">
-                                <img src={IconDownloadWhite} alt="" />
-                                <h5>
-                                    YOUR ANNUAL SUMMARY
-                                </h5>
-                            </button>
-                        </div>
-                    </div>
+                    <Sales />
                 </div>
                 <div className="report-summary">
-                    <div className="report-summary-insights">
-                        <h5 className="report-summary-insights-title">
-                            Insights
-                        </h5>
-                        <Divider direction={"vertical"} />
-                        <div className="report-summary-insights-details">
-                            <div className="report-summary-insights-details-text">
-                                <h6>
-                                    21,214
-                                </h6>
-                                <p>
-                                    Last Week
-                                </p>
-                            </div>
-                            <Divider direction={"horizontal"} />
-                            <div className="report-summary-insights-details-text">
-                                <h6>
-                                    21,214
-                                </h6>
-                                <p>
-                                    Last Week
-                                </p>
-                            </div>
-                            <Divider direction={"horizontal"} />
-                            <div className="report-summary-insights-details-text">
-                                <h6>
-                                    21,214
-                                </h6>
-                                <p>
-                                    Last Week
-                                </p>
-                            </div>
-                        </div>
-                        <Divider direction={"vertical"} />
-                        <div className="report-summary-insights-product">
-                            <h6 className="report-summary-insights-product-title">
-                                Most Popular Product
-                            </h6>
-                            <div className="report-summary-insights-product-content">
-                                <img src={ImageOil} alt="" />
-                                <p>
-                                    Pomegranate Juice Pet Bottle, Best material and design, Ready to Order
-                                </p>
-                            </div>
-                        </div>
-                        <div className="report-summary-insights-product">
-                            <h6 className="report-summary-insights-product-title">
-                                Most Popular Excess Capacity
-                            </h6>
-                            <div className="report-summary-insights-product-content">
-                                <img src={ImageOil} alt="" />
-                                <p>
-                                    Pomegranate Juice Pet Bottle, Best material and design, Ready to Order
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="report-summary-chart">
-                        <ComponentLineChart />
-                    </div>
+                    <SummaryPage />
                 </div>
                 <div className="report-statistics">
-                    <div className="report-statistics-activity">
-                        <ActivityChart
-                            datalist1={[
-                                50, 80, 250, 200, 500, 700, 100, 625, 145, 500, 1236, 522,
-                            ]}
-                            datalist2={[
-                                100, 200, 300, 400, 500, 600, 700, 800, 900, 1000, 1100, 1200,
-                            ]}
-                        />
-                    </div>
-                    <div className="report-statistics-details">
-                        <div className="report-statistics-details-chart">
-                            <div className="w-50">
-                                <span>Sales </span>
-                                <ChartDoughnut dataChart={["152423", "242423"]} />
-                            </div>
-                            <div className="w-50">
-                                <span>Purchase</span>
-                                <ChartDoughnut dataChart={["152423", "242423"]} />
-                            </div>
-                        </div>
-                        <div className="report-statistics-details-keyword">
-                            <h5 className="report-statistics-details-keyword-title">
-                                Top Keywords
-                            </h5>
-                            <div className="report-statistics-details-keyword-button">
-                                {this.KEYWORD.map((name, index) => (
-                                    <ButtonOutline key={index} title={name} />
-                                ))}
-                            </div>
-                        </div>
-                    </div>
+                    <Statistics />
                 </div>
             </div>
         )
     }
+
 }
 
 export default Report;
---
+++ src/views/pages/report/sales.tsx
@@ -0,0 +1,134 @@
+import React from "react";
+import { Component } from "react";
+import { IconArrow, IconDownload, IconDownloadWhite, IconUpload, ImageBox, ImageChat, ImageOil, ImageShop, ImageTwoUser, ImageWallet, ImageWorld, Iran } from "../../../assets";
+import ActivityChart from "../../components/activity_chart";
+import { ButtonOutline, ChartDoughnut, Divider, Summary, TextIconVertical } from "../../components/components";
+import ComponentLineChart from "../../components/component_line_chart";
+import DashboardNav from "../../components/dashboard_nav";
+import ReportController from "../../controllers/report_controller";
+
+class Sales extends ReportController {
+    render() {
+        return (
+            <>
+                <div className="report-sales-countries">
+                    <div className="report-sales-countries-map">
+                        <h4>
+                            Most Sales in Countries
+                        </h4>
+                        <img src={ImageWorld} alt="" />
+                    </div>
+                    <div className="report-sales-countries-divider">
+                        <Divider direction={"horizontal"} />
+                    </div>
+                    <div className="report-sales-countries-flag">
+                        <h6>
+                            Top Countries
+                        </h6>
+                        <div className="report-sales-countries-flag-top">
+                            <div className="report-sales-countries-flag-top-name">
+                                <img src={Iran} alt="" />
+                                <h6>
+                                    Canada
+                                </h6>
+                            </div>
+                            <h5 className="report-sales-countries-flag-top-sale">
+                                <span>$</span>
+                                152,423
+                            </h5>
+                        </div>
+                        <div className="report-sales-countries-flag-top">
+                            <div className="report-sales-countries-flag-top-name">
+                                <img src={Iran} alt="" />
+                                <h6>
+                                    Canada
+                                </h6>
+                            </div>
+                            <h5 className="report-sales-countries-flag-top-sale">
+                                <span>$</span>
+                                152,423
+                            </h5>
+                        </div>
+                        <div className="report-sales-countries-flag-top">
+                            <div className="report-sales-countries-flag-top-name">
+                                <img src={Iran} alt="" />
+                                <h6>
+                                    Canada
+                                </h6>
+                            </div>
+                            <h5 className="report-sales-countries-flag-top-sale">
+                                <span>$</span>
+                                152,423
+                            </h5>
+                        </div>
+                        <div className="report-sales-countries-flag-top">
+                            <div className="report-sales-countries-flag-top-name">
+                                <img src={Iran} alt="" />
+                                <h6>
+                                    Canada
+                                </h6>
+                            </div>
+                            <h5 className="report-sales-countries-flag-top-sale">
+                                <span>$</span>
+                                152,423
+                            </h5>
+                        </div>
+                        <div className="report-sales-countries-flag-top">
+                            <div className="report-sales-countries-flag-top-name">
+                                <img src={Iran} alt="" />
+                                <h6>
+                                    Canada
+                                </h6>
+                            </div>
+                            <h5 className="report-sales-countries-flag-top-sale">
+                                <span>$</span>
+                                152,423
+                            </h5>
+                        </div>
+                        <div className="report-sales-countries-flag-top">
+                            <div className="report-sales-countries-flag-top-name">
+                                <img src={Iran} alt="" />
+                                <h6>
+                                    Canada
+                                </h6>
+                            </div>
+                            <h5 className="report-sales-countries-flag-top-sale">
+                                <span>$</span>
+                                152,423
+                            </h5>
+                        </div>
+                    </div>
+                </div>
+                <div className="report-sales-download">
+                    <div className="report-sales-download-report">
+                        <h5 className="report-sales-download-report-title">
+                            Download Reports
+                        </h5>
+                        <p className="report-sales-download-report-description">
+                            Download Touba’s last year sales and marketing digest. It can help you to make the right decisions
+                        </p>
+                        <button className="report-sales-download-report-button">
+                            <img src={IconDownload} alt="" />
+                            <h5>
+                                TOUBA`S ANNUAL REPORT
+                            </h5>
+                        </button>
+                    </div>
+                    <div className="report-sales-download-summary">
+                        <p className="report-sales-download-summary-description">
+                            Download your annual sell/buy summary here. You can analaysis your products here.
+                        </p>
+                        <button className="report-sales-download-summary-button">
+                            <img src={IconDownloadWhite} alt="" />
+                            <h5>
+                                YOUR ANNUAL SUMMARY
+                            </h5>
+                        </button>
+                    </div>
+                </div>
+            </>
+        )
+    }
+}
+
+export default Sales;
