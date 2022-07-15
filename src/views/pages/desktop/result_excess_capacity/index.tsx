2fe0a06e71107129c877ddb5e8c585e8bf505f17
MRezaHeydar1376 <103419484+MRezaHeydar1376@users.noreply.github.com>
2022-07-15 15:15:08 -0700

    Add <create controller to page ResultExcessCapacity>

--- src/views/pages/desktop/result_excess_capacity/index.tsx
+++ src/views/pages/desktop/result_excess_capacity/index.tsx
@@ -1,33 +1,14 @@
-import { BaseComponent } from "utilities_js";
 import { ImageTest } from "../../../../assets";
 import { CardOfferHorizontal } from "../../../components/cars_offer_horizontal";
 import { ComponentFilterTopbar, ComponentFilterSidebar } from "../../../components/components";
 import Component_search_bar from "../../../components/component_search_bar";
+import ResultExcessCapacityController from "../../../controllers/result_excess_capacity_controller";
 import BottomNav from "../../../layout/bottom_nav";
 import Footer from "../../../layout/footer";
 import TopNav from "../../../layout/top_nav";
 
-interface IProps {}
-
-interface IState {
-  fakeList: any;
-}
-
-export class SearchResultExcessCapacity extends BaseComponent<IProps, IState> {
-  TEST = [
-    { name: "JAN", size: 20 },
-    { name: "FEB", size: 30 },
-    { name: "MAR", size: 40 },
-    { name: "APR", size: 50 },
-    { name: "MAY", size: 60 },
-    { name: "JUN", size: 70 },
-    { name: "JUL", size: 80 },
-    { name: "AUG", size: 90 },
-    { name: "SEP", size: 100 },
-    { name: "OCT", size: 20 },
-    { name: "NOV", size: 20 },
-    { name: "DEC", size: 20 },
-  ];
+export class SearchResultExcessCapacity extends ResultExcessCapacityController {
+  
   render() {
     return (
       <div>
