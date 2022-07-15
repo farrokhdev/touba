9cce3674fc9c0d4802790e7c47f05e54c5cb2774
MRezaHeydar1376 <103419484+MRezaHeydar1376@users.noreply.github.com>
2022-07-15 15:31:56 -0700

    Add <create controller to page Supplier>

--- src/views/pages/desktop/supplier/index.tsx
+++ src/views/pages/desktop/supplier/index.tsx
@@ -1,47 +1,15 @@
-import React, { Component } from "react";
-import { BaseComponent } from "utilities_js";
-import { CardOfferHorizontal } from "../../../components/cars_offer_horizontal";
-import Component_search_bar from "../../../components/component_search_bar";
-import BottomNav from "../../../layout/bottom_nav";
-import Footer from "../../../layout/footer";
-import TopNav from "../../../layout/top_nav_home";
-import image_test from "../../../assets/images/image-test.png";
-import {
-  CardLike,
-  ComponentCapicityFilter,
-  ComponentFilterRangeSlider,
-  ComponentFilterSidebar,
-  ComponentSuppliers,
-  SelectComponent,
-  SelectText,
-} from "../../../components/components";
 import { Link } from "react-router-dom";
-interface IProps {}
+import { ComponentFilterSidebar, SelectComponent, ComponentCapicityFilter, ComponentSuppliers } from "../../../components/components";
+import SupplierController from "../../../controllers/supplier_controller";
+import Footer from "../../../layout/footer";
 
-interface IState {
-  fakeList: any;
-}
-
-export class ToubaSupplier extends BaseComponent<IProps, IState> {
-  TEST = [
-    { name: "JAN", size: 20 },
-    { name: "FEB", size: 30 },
-    { name: "MAR", size: 40 },
-    { name: "APR", size: 50 },
-    { name: "MAY", size: 60 },
-    { name: "JUN", size: 70 },
-    { name: "JUL", size: 80 },
-    { name: "AUG", size: 90 },
-    { name: "SEP", size: 100 },
-    { name: "OCT", size: 20 },
-    { name: "NOV", size: 20 },
-    { name: "DEC", size: 20 },
-  ];
+export class ToubaSupplier extends SupplierController {
+  
   render() {
     return (
       <div>
         <div className=" px-5">
-          <div className="row p-3      ">
+          <div className="row p-3">
             <div className="col-3">
               <ComponentFilterSidebar showButton={true} />
             </div>
